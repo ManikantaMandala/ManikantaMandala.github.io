3bdb278b729fe2dcc22e1c7d0e3c7560ba7aaf9c
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2024-02-03 16:53:26 +0530

    added images and changed BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import './App.css'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import NavBar from './components/NavBar';
 import Projects from './components/routes/Projects';
 import Resume from './components/routes/Resume';
@@ -8,32 +8,32 @@ import Terminal from './components/routes/Terminal';
 import Footer from './components/Footer';
 
 function App() {
-  return (
-      <div className='App'>
-          <BrowserRouter>
-              <NavBar />
-              <Routes>
-                  <Route
-                      path='/'
-                      element=<Home/>
-                  />
-                  <Route
-                      path='/projects'
-                      element=<Projects/>
-                  />
-                  <Route
-                      path='/terminal'
-                      element=<Terminal/>
-                  />
-                  <Route
-                      path='/resume'
-                      element=<Resume/>
-                  />
-              </Routes>
-              <Footer/>
-          </BrowserRouter>
-      </div>
-  )
+    return (
+        <div className='App'>
+            <Router basename='/'>
+                <NavBar />
+                <Routes>
+                    <Route 
+                        path='/' 
+                        element={<Home />} 
+                    />
+                    <Route 
+                        path='/projects' 
+                        element={<Projects />} 
+                    />
+                    <Route 
+                        path='/terminal' 
+                        element={<Terminal />} 
+                    />
+                    <Route 
+                        path='/resume' 
+                        element={<Resume />} 
+                    />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
-export default App
+export default App;
